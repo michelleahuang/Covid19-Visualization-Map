ddcb00f2c9c256ace90ef76719db7d7a64beae91
Michelle Huang <michelleahuang@berkeley.edu>
2021-12-31 10:51:50 -0800

    Add event litener on instructions box

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Globe from "./scripts/globe.js";
+import instructions from "./scripts/instructions.js";
 
 const canvas = document.getElementById("globe-canvas")
 new Globe(canvas);
+instructions();
---
+++ src/scripts/instructions.js
@@ -0,0 +1,9 @@
+export default function instructions () {
+    let instructionsButton = document.getElementById("instructions-button");
+
+    instructionsButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        let instructionsBox = document.getElementById("instructions");
+        instructionsBox.classList.toggle("instructions-box-hidden")
+    })
+}
