e96e965e5d3555edc0e7b96d627b38541573c1c6
Michelle Huang <michelleahuang@berkeley.edu>
2021-12-30 16:11:04 -0800

    Create globe's shape

--- src/scripts/globe.js
+++ src/scripts/globe.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const CONSTANTS = {
-    WIDTH: window.innerWidth,
-    HEIGHT: window.innerHeight
+    WIDTH: 640,
+    HEIGHT: 720
 }
 
 export default class Globe {
@@ -15,20 +15,46 @@ export default class Globe {
         renderer.setSize(CONSTANTS.WIDTH, CONSTANTS.HEIGHT);
         renderer.setPixelRatio(window.devicePixelRatio);
 
-        // Create Controls
-        const controls = new OrbitControls(camera, canvas);
-
         // Create Globe
+        const globeMap =new THREE.TextureLoader().load("src/images/earthmap.jpeg");
+        const globeBumpMap = new THREE.TextureLoader().load("src/images/earthbump.jpeg");
+        const globeSpecularMap = new THREE.TextureLoader().load("src/images/earthspec.jpeg");
+        
+        const globeGeometry = new THREE.SphereGeometry(10, 32, 32); // radius = 10
+        const globeMaterial = new THREE.MeshPhongMaterial({
+            map: globeMap, 
+            bumpMap: globeBumpMap,
+            bumpScale: 0.10,
+            specularMap: globeSpecularMap,
+            specular: new THREE.Color('grey')
+        })
+        const globe = new THREE.Mesh(globeGeometry, globeMaterial);
 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+        // Create Lights
+        const lights = [];
 
-        camera.position.z = 5;
+        // Create Clouds
+
+
+        // Add Globe to Scene
+        scene.add(globe);
+
+
+        // // Create Controls
+        const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
+
+
+        camera.position.z = 15;
+
+        // Functions 
 
         function animate() {
             requestAnimationFrame( animate );
+            controls.update();
+            globe.rotation.y += 0.001;
             renderer.render( scene, camera );
         }
         animate();
